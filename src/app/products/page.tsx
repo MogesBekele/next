c4926977e6d8a150128ad58d70e05870695eed44
MogesBekele <mogesbekele32@gmail.com>
2025-05-19 14:52:29 +0300

    complete the navigation tutor

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,22 +1,13 @@
+import Link from "next/link";
+export default function ProductComponent() {
+  return (
+    <div>
+      <Link href="/"> Home</Link>
 
-import Link from "next/link"
-export default function ProductComponent(){
-
-
-return(
-
-  <div>
-    <Link href="/">  Home</Link>
-    
-     <Link href="/products/1">  product 1</Link>
-       <Link href="/products/2">  product 2</Link>
-         <Link href="/products/3">  product 3</Link>
-           <Link href="/products/4">  product 4</Link>
-    
-
-  </div>
-)
-
-
-
+      <Link href="/products/1"> product 1</Link>
+      <Link href="/products/2"> product 2</Link>
+      <Link href="/products/3"> product 3</Link>
+      <Link href="/products/4"> product 4</Link>
+    </div>
+  );
 }
