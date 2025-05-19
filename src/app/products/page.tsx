9be3dd072e774d8c82674c307a6dbaf15a6eae63
MogesBekele <mogesbekele32@gmail.com>
2025-05-19 14:45:54 +0300

    create the dynamic route

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,6 +8,8 @@ return(
   <div>
     <Link href="/">  Home</Link>
     
+     <Link href="/products/1">  product 1</Link>
+    
   <h1>this is product component 1</h1>
    <h1>this is product component 2</h1>
     <h1>this is product component 3</h1>
