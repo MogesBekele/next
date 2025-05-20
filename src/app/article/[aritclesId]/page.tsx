b0ad8b60cc04e2f77d9d7af059503b991d1c6a37
MogesBekele <mogesbekele32@gmail.com>
2025-05-20 12:30:14 +0300

    add the link on articles component

--- src/app/article/[aritclesId]/page.tsx
+++ src/app/article/[aritclesId]/page.tsx
@@ -1,9 +1,14 @@
-export default function Aritcles(){
-
-  return <div>
-
-    <h1>news article id</h1>
-    <h1>reading in language</h1>
-  </div>
-
+import Link from "next/link";
+export default function Aritcles() {
+  return (
+    <div>
+      <h1>news article id</h1>
+      <h1>reading in language</h1>
+      <div>
+        <Link href="/articles/id?lang=en">English</Link>
+        <Link href="articles/id?lang=spa">spanish</Link>
+        <Link href="/articles/id/lang=fr">french</Link>
+      </div>
+    </div>
+  );
 }
