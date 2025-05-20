cbaae6f3c79f6216698ea3570bd447090c78cd46
MogesBekele <mogesbekele32@gmail.com>
2025-05-20 12:23:06 +0300

    learn about params and search params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
   <h1>wellcome to home page</h1>
   <Link href="/blog/first">blog</Link>
   <Link href='/products'>product</Link>
+  <Link href='articles/breaking-news-123?lang=en'>read in english</Link>
+   <Link href='articles/breaking-news-123?lang=en
+   fr'>read in french</Link>
+
+
 
   
   </>
