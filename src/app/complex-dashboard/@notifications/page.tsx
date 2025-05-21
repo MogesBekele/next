898e989ca377158e9378c170abe58c69a44f9dc6
MogesBekele <mogesbekele32@gmail.com>
2025-05-21 13:02:30 +0300

    create the archived folder on the notificatons folder

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,9 +1,15 @@
 import { Cart } from "@/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
   return (
     <Cart>
       <h1>Notifications</h1>
+      <div>
+        <Link href="/complex-dashboard/archived">Archived</Link>
+        <br />
+    
+      </div>
     </Cart>
   );
 }
