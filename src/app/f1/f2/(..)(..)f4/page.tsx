bcba28ee904c4e5608a59b6c173754d780c41613
MogesBekele <mogesbekele32@gmail.com>
2025-05-21 18:02:19 +0300

    create intercept folder

---
+++ src/app/f1/f2/(..)(..)f4/page.tsx
@@ -0,0 +1,7 @@
+export default function InterceptedRoute() {
+  return (
+    <div>
+      <h1>(..)(..)Intercepted Route</h1>
+    </div>
+  );
+}
--- src/app/f1/f2/page.tsx
+++ src/app/f1/f2/page.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 export default function F2(){
 
   return (
     <div>
-      <h1>F2</h1>
+      <h1>F2 <Link href={'/f4'}>f4</Link> </h1>
     </div>
   );
 }
