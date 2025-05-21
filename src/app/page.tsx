72dde5eb224995bdf281aaa5ea27721437b0e053
MogesBekele <mogesbekele32@gmail.com>
2025-05-21 11:42:06 +0300

    fix the hoome page bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,32 @@
 import Link from "next/link";
 
-
 export default async function Home() {
-  await new Promise(resolve=>{
-    setTimeout(()=>{resolve('delay')},2000)
-  })
+  // Simulate loading delay
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  
   return (
-    <>
-      <h1>Welcome to home page</h1>
-
-      <Link href="/blog/first">Blog</Link>
-      <br />
-      <Link href="/products">Product</Link>
-      <br />
-    <Link href='/article/breaking-news-123?lang=en'>read in English</Link>
-
-      <br />
-     <Link href='/article/breaking-news-123?lang=fr'>read in French</Link>
-
-    </>
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to the Home Page</h1>
+      <nav aria-label="Main navigation">
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          <li>
+            <Link href="/blog/first">Blog</Link>
+          </li>
+          <li>
+            <Link href="/products">Products</Link>
+          </li>
+          <li>
+            <Link href="/article/breaking-news-123?lang=en">
+              Read in English
+            </Link>
+          </li>
+          <li>
+            <Link href="/article/breaking-news-123?lang=fr">
+              Read in French
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
   );
 }
