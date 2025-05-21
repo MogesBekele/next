bcc5ef7dd1067747f3ee582927496e30d3a458c4
MogesBekele <mogesbekele32@gmail.com>
2025-05-21 17:57:31 +0300

    intercetp route

--- src/app/f1/page.tsx
+++ src/app/f1/page.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function F1(){
   return (
     <div>
-      <h1>F1</h1>
+      <h1>F1 page</h1>
       <Link href={"/f1/f2"}>f2</Link>
+      <Link href={'/f3'}>f3</Link>
     </div>
   );
 }
