9a24faa3cb0b733d491a0d5022e9a7d462522ca4
MogesBekele <mogesbekele32@gmail.com>
2025-05-21 13:15:39 +0300

    add the default file on the unmatched routes

---
+++ src/app/complex-dashboard/@revenue/default.tsx
@@ -0,0 +1,9 @@
+import { Cart } from "@/components/card";
+
+export default function RevenueMatrics() {
+  return (
+    <Cart>
+      <h1>RevenueMatrics default</h1>
+    </Cart>
+  );
+}
---
+++ src/app/complex-dashboard/@users/default.tsx
@@ -0,0 +1,10 @@
+
+import { Cart } from "@/components/card";
+
+export default function UsersAnalytics() {
+  return (
+    <Cart>
+      <h1>UsersAnalytics default</h1>
+    </Cart>
+  );
+}
